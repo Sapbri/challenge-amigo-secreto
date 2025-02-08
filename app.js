f4ed8e8e9e62fea637b99b42b11a3d8edd710af2
Sapbri <92655888+Sapbri@users.noreply.github.com>
2025-02-08 18:46:10 -0500

    Actualiza app.js. Implementa función para sortear amigos.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 // Array Amigos
-let amigos = [];
+const amigos = [];
 
 // Función agregar amigo desde el input.
 function agregarAmigo() {
   let nombreAmigo = document.getElementById('amigo').value;
   if (nombreAmigo == "") {
     alert('Por favor, inserte un nombre.');
+  } else if (amigos.includes(nombreAmigo)) {
+    alert('Ese nombre ya fue registrado.')
   } else {
     amigos.push(nombreAmigo);
-    asignarTextoListaAmigos(nombreAmigo);
+    asignarTextoListaAmigos();
   }
-  console.log(amigos);
   limpiarInput();
 }
 
@@ -19,11 +20,30 @@ function asignarTextoListaAmigos() {
   let listaAmigoSecreto = document.getElementById('listaAmigos');
   listaAmigoSecreto.innerHTML = '';
   for (let i = 0; i < amigos.length; i++) {
-    const li = document.createElement('li');
-    textoNombreAmigo = amigos[i];
+    let li = document.createElement('li');
+    let textoNombreAmigo = amigos[i];
     li.appendChild(document.createTextNode(textoNombreAmigo));
     listaAmigoSecreto.appendChild(li);
   }
+}
+
+// Función sortear amigo, para mostrar el nombre ganador del sorteo del amigo secreto.
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert('Por favor, añada un nombre.')
+  } else {
+    limpiarListaAmigos()
+    let indiceGenerado = Math.floor(Math.random()*amigos.length);
+    let amigoGanador = amigos[indiceGenerado];
+    let ganadorHTML = document.getElementById('resultado');
+    ganadorHTML.innerHTML = (`El amigo secreto sorteado es: ${amigoGanador}`);
+  }
+}
+
+//Función para limpiar nuestra lista de amigos en la parte visual.
+function limpiarListaAmigos() {
+  let listaAmigo = document.getElementById('listaAmigos');
+  listaAmigo.innerHTML = ''; 
 }
 
 // Función para limpiar nuestro input, cada vez que clickeamos nuestro botón 'Añadir'.
