703b5757e248e15744db20ec836f9aabf5d0f89f
Sapbri <92655888+Sapbri@users.noreply.github.com>
2025-02-08 17:22:35 -0500

    Actualiza app.js. Implementa función para actualizar lista de amigos

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+// Array Amigos
 let amigos = [];
 
+// Función agregar amigo desde el input.
 function agregarAmigo() {
   let nombreAmigo = document.getElementById('amigo').value;
   if (nombreAmigo == "") {
     alert('Por favor, inserte un nombre.');
   } else {
     amigos.push(nombreAmigo);
+    asignarTextoListaAmigos(nombreAmigo);
   }
   console.log(amigos);
   limpiarInput();
 }
 
+// Función para mostrar los nombres de nuestros amigos secretos, cada uno en un elemeto li. Se limpia la lista para evitar los nombres duplicados al actualizar.
+function asignarTextoListaAmigos() {
+  let listaAmigoSecreto = document.getElementById('listaAmigos');
+  listaAmigoSecreto.innerHTML = '';
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement('li');
+    textoNombreAmigo = amigos[i];
+    li.appendChild(document.createTextNode(textoNombreAmigo));
+    listaAmigoSecreto.appendChild(li);
+  }
+}
+
+// Función para limpiar nuestro input, cada vez que clickeamos nuestro botón 'Añadir'.
 function limpiarInput() {
   document.querySelector('#amigo').value = '';
 }
